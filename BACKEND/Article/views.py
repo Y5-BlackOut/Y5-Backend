cf58a8fb533b6af2cbfef58e689c1cfedd3178c6
Hoyoung027 <hybyun0207@gmail.com>
2025-01-12 02:52:00 +0900

    news create api complete

--- BACKEND/Article/views.py
+++ BACKEND/Article/views.py
@@ -19,12 +19,9 @@ class NewsViewSet(ViewSet):
         # 요청 데이터에서 해당 글의 reference id 리스트 추출
         references = request.data.get('reference', [])  
         
-
         # Request 데이터에서 Serializer를 생성
         serializer = NewsSerializer(data=request.data)
         
-        # 데이터 유효성 검사
-        if serializer.is_valid():
         # 데이터 유효성 검사
         if serializer.is_valid():
 
@@ -60,3 +57,20 @@ class NewsViewSet(ViewSet):
             instance = serializer.save(transactionHash=transactionHash)
             return Response(NewsSerializer(instance).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # 유효성 오류 응답
+
+
+class BlogPostViewSet(ViewSet):
+    queryset = BlogPost.objects.all()
+    def list(self, request):
+        """모든 BlogPost 글을 조회"""
+        blogs = BlogPost.objects.all()
+        serializer = BlogPostSerializer(blogs, many=True)
+        return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        """특정 id로 BlogPost 글 조회"""
+        try:
+            blog = BlogPost.objects.get(pk=pk)
+            serializer = BlogPostSerializer(blog)
+            return Response(serializer.data)
+        except BlogPost.DoesNotExist:
+            return Response({"error": "Blog not found"}, status=404)
