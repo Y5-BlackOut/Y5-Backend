8b2afe58221f487df3ac503695693fdfadb02cc9
Hoyoung027 <hybyun0207@gmail.com>
2025-01-12 02:34:04 +0900

    new article api complete

--- BACKEND/Article/urls.py
+++ BACKEND/Article/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import *
 
-# # ViewSet의 'list' 메서드를 GET 요청에 매핑
-# news_list = NewsViewSet.as_view({
-#     'post': 'create',
-# })
+# ViewSet의 'list' 메서드를 GET 요청에 매핑
+news_list = NewsViewSet.as_view({
+    'post': 'create',
+})
 
-# urlpatterns = [
-#     path('news', news_list, name='article-list'),
-# ]
+urlpatterns = [
+    path('news', news_list, name='article-list'),
+]
